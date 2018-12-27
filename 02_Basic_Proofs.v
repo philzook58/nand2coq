ea2e1a7a21559f47fb65ac6143b4737615cd7bc0
philzook58 <philzook58@gmail.com>
2018-12-26 23:17:02 -0500

    started at least listing important tactics

--- 02_Basic_Proofs.v
+++ 02_Basic_Proofs.v
@@ -28,4 +28,103 @@ Equality.
 
 LTAC?
 
+
+
+
+Goal
+
 *)
+(*
+https://stackoverflow.com/questions/32682544/is-there-a-minimal-complete-set-of-tactics-in-coq
+
+https://pjreddie.com/coq-tactics/
+http://adam.chlipala.net/itp/tactic-reference.html
+
+
+
+
+Automation
+
+auto - automatic proof search
+ring - solves polynomials / numbers stuff.
+omega - automatic solves simple problems for integers
+
+
+
+exact - give exactly the term that fulfills the proof obligations
+refine - give proof with holes
+
+intro{s} intro introduces variables. 
+
+reflexivity - When the goal is an obvious equality
+
+simpl - does computational simplification
+
+
+exists, , symmetry, 
+apply, rewrite, revert, destruct and induction. inversion
+
+
+
+Tacticals
+
+repeat
+try 
+;
+
+
+*)
+
+About prod.
+
+Theorem myfst (a b : Type): (prod a b) ->  a.
+Proof.
+  exact fst.
+Qed.
+
+
+Search and.
+Search exist.
+Theorem twoexists : exists x : nat, 2 = x.
+Proof.
+  exists 2. reflexivity.
+Qed.
+
+Theorem twoexists' : exists x : nat, 2 = x.
+Proof.
+  eauto. 
+Qed.
+
+Search nat.
+
+Theorem twoeven : Nat.even 2 = true.
+Proof.
+  reflexivity.
+Qed.
+
+Definition square (x : nat) : nat := x * x.
+Compute square 4.
+
+Search nat.
+
+
+
+(*
+Theorem squareven : forall x : nat, Nat.even (square x) = true.
+Proof.
+  intro x. induction x. simpl. reflexivity.
+  unfold square. 
+ *)
+(*
+Theorem eveneven : forall x y: nat, Nat.even x = true ->  Nat.even y = true -> Nat.even (x + y) = true.
+Proof.
+  intros x y Hx Hy. induction x. simpl. apply Hy. 
+ *)
+(*
+Theorem eveneven : forall x: nat, Nat.even (x + x) = true.
+Proof.
+  intros x. induction x. simpl. reflexivity.  simpl.
+ *)
+Theorem eveneven : forall x: nat, Nat.even x = true -> Nat.even (S x) = false.
+Proof.
+  intros x. 
